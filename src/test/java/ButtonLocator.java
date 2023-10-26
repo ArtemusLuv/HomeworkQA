cc4ffe3d530e954884c83aed2c137ed849713a67
Артем Никитин <117100565+ArtemusLuv@users.noreply.github.com>
2023-10-26 19:22:23 +0500

    Add files via upload

---
+++ src/test/java/ButtonLocator.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+
+public class ButtonLocator {
+    protected static By number_1 = By.id("com.google.android.calculator:id/digit_1");
+    protected static By number_2 = By.id("com.google.android.calculator:id/digit_2");
+    protected static By number_3 = By.id("com.google.android.calculator:id/digit_3");
+    protected static By number_4 = By.id("com.google.android.calculator:id/digit_4");
+    protected static By number_5 = By.id("com.google.android.calculator:id/digit_5");
+    protected static By number_6 = By.id("com.google.android.calculator:id/digit_6");
+    protected static By number_7 = By.id("com.google.android.calculator:id/digit_7");
+    protected static By number_8 = By.id("com.google.android.calculator:id/digit_8");
+    protected static By number_9 = By.id("com.google.android.calculator:id/digit_9");
+    protected static By number_0 = By.id("com.google.android.calculator:id/digit_0");
+    protected static By plus = By.id("com.google.android.calculator:id/op_add");
+    protected static By minus = By.id("com.google.android.calculator:id/op_sub");
+    protected static By divide = By.id("com.google.android.calculator:id/op_div");
+    protected static By multiply = By.id("com.google.android.calculator:id/op_mul");
+    protected static By equals = By.id("com.google.android.calculator:id/eq");
+    protected static By window_result = By.id("com.google.android.calculator:id/result_final");
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,113 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+    static AndroidDriver <AndroidElement> driver = null;
+
+//    protected By number_1 = By.id("com.google.android.calculator:id/digit_1");
+//    protected By number_2 = By.id("com.google.android.calculator:id/digit_2");
+//    protected By number_3 = By.id("com.google.android.calculator:id/digit_3");
+//    protected By number_4 = By.id("com.google.android.calculator:id/digit_4");
+//    protected By number_5 = By.id("com.google.android.calculator:id/digit_5");
+//    protected By number_6 = By.id("com.google.android.calculator:id/digit_6");
+//    protected By number_7 = By.id("com.google.android.calculator:id/digit_7");
+//    protected By number_8 = By.id("com.google.android.calculator:id/digit_8");
+//    protected By number_9 = By.id("com.google.android.calculator:id/digit_9");
+//    protected By number_0 = By.id("com.google.android.calculator:id/digit_0");
+//    protected By plus = By.id("com.google.android.calculator:id/op_add");
+//    protected By equals = By.id("com.google.android.calculator:id/eq");
+//    protected By window = By.id("com.google.android.calculator:id/result_final");
+
+    @BeforeAll
+    public static void initialize () {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nothing phone(1)");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        try {
+            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @DisplayName("Тест кейс операции сложения")
+    @Description("Складываем 1 и 2, ожидаемо получить 3")
+    @Test
+    public void plusTest () {
+        driver.findElement(ButtonLocator.number_1).click();
+        driver.findElement(ButtonLocator.plus).click();
+        driver.findElement(ButtonLocator.number_2).click();
+        driver.findElement(ButtonLocator.equals).click();
+
+        // 1 + 2 = 3
+        assertEquals("3", driver.findElement(ButtonLocator.window_result).getText());
+    }
+
+    @DisplayName("Тест кейс операции вычитания")
+    @Description("Из 10 вычитаем 9, ожидаемо получить 1")
+    @Test
+    public void minusTest () {
+        driver.findElement(ButtonLocator.number_1).click();
+        driver.findElement(ButtonLocator.number_0).click();
+        driver.findElement(ButtonLocator.minus).click();
+        driver.findElement(ButtonLocator.number_9).click();
+        driver.findElement(ButtonLocator.equals).click();
+
+        // 10 - 9 = 1
+        assertEquals("1", driver.findElement(ButtonLocator.window_result).getText());
+    }
+
+    @DisplayName("Тест кейс операции умножения")
+    @Description("Дважды умножаем на 9, затем умножаем на 0, ожидаемо получить 0")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test
+    public void multiplyTest () {
+        driver.findElement(ButtonLocator.number_9).click();
+        driver.findElement(ButtonLocator.multiply).click();
+        driver.findElement(ButtonLocator.number_9).click();
+        driver.findElement(ButtonLocator.multiply).click();
+        driver.findElement(ButtonLocator.number_0).click();
+        driver.findElement(ButtonLocator.equals).click();
+
+        // 9 * 9 * 0 = 0
+        assertEquals("0", driver.findElement(ButtonLocator.window_result).getText());
+    }
+
+    @DisplayName("Тест кейс операции деления")
+    @Description("Делим 50 на 4, ожидаемо получить 12.5")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test
+    public void divideTest () {
+        driver.findElement(ButtonLocator.number_5).click();
+        driver.findElement(ButtonLocator.number_0).click();
+        driver.findElement(ButtonLocator.divide).click();
+        driver.findElement(ButtonLocator.number_4).click();
+        driver.findElement(ButtonLocator.equals).click();
+
+        // 50 / 4 = 12.5
+        assertEquals("12.5", driver.findElement(ButtonLocator.window_result).getText());
+    }
+
+    @AfterAll
+    public static void tearDown () {
+        driver.quit();
+    }
+}
